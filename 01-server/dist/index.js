e56236b50744adaea5c97756dd6ac5210dfe6041
luis manuel <luisyosemite@gmail.com>
2018-11-29 00:43:07 -0600

    routes and cors

--- 01-server/dist/index.js
+++ 01-server/dist/index.js
@@ -4,7 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./class/server"));
+const router_1 = __importDefault(require("./routers/router"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 const server = new server_1.default();
+//BODYPARSER
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+//CORS
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+//RUTES
+server.app.use('/', router_1.default);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port}`);
 });
--- 01-server/index.ts
+++ 01-server/index.ts
@@ -1,7 +1,18 @@
 import Server from './class/server'
+import router from './routers/router'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const server = new Server()
+//BODYPARSER
+server.app.use( bodyParser.urlencoded( { extended:true } )  )
+server.app.use( bodyParser.json() )
+//CORS
+server.app.use( cors( { origin:true, credentials:true } ) )
+//RUTES
+server.app.use('/', router)
 
 server.start( () => {
     console.log(`Servidor corriendo en el puerto ${ server.port }`)
 })
+
