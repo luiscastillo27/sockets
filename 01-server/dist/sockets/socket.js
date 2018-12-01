c0a5516f5af111b5f43379a474e40d7b35dbab9f
luis manuel <luisyosemite@gmail.com>
2018-12-01 00:51:00 -0600

    .

---
+++ 01-server/dist/sockets/socket.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.desconectar = (cliente) => {
+    cliente.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+};
+// Escuchar mensajes
+exports.mensaje = (cliente, io) => {
+    cliente.on('mensaje', (payload) => {
+        console.log('Mensaje recibido', payload);
+        io.emit('mensaje-nuevo', payload);
+    });
+};
