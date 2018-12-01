0db20e69bbf6a03cb9cf655afa2b163c94dd03d6
luis manuel <luisyosemite@gmail.com>
2018-12-01 00:49:17 -0600

    basic finish

--- 01-server/dist/index.js
+++ 01-server/dist/index.js
@@ -3,17 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./class/server"));
-const router_1 = __importDefault(require("./routers/router"));
+const server_1 = __importDefault(require("./classes/server"));
+const router_1 = __importDefault(require("./routes/router"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const server = server_1.default.instance;
-//BODYPARSER
+// BodyParser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
-//CORS
+// CORS
 server.app.use(cors_1.default({ origin: true, credentials: true }));
-//RUTES
+// Rutas de servicios
 server.app.use('/', router_1.default);
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port}`);
--- 01-server/index.ts
+++ 01-server/index.ts
@@ -1,18 +1,28 @@
-import Server from './class/server'
-import router from './routers/router'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const server = Server.instance
-//BODYPARSER
-server.app.use( bodyParser.urlencoded( { extended:true } )  )
-server.app.use( bodyParser.json() )
-//CORS
-server.app.use( cors( { origin:true, credentials:true } ) )
-//RUTES
-server.app.use('/', router)
+
+
+const server = Server.instance;
+
+// BodyParser
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+// CORS
+server.app.use( cors({ origin: true, credentials: true  }) );
+
+
+// Rutas de servicios
+server.app.use('/', router );
+
+
+
 
 server.start( () => {
-    console.log(`Servidor corriendo en el puerto ${ server.port }`)
-})
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+});
+
 
