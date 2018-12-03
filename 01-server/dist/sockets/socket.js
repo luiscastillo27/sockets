8019b7c688e8b9878788a2f31c64f10d3dc8e9b7
luis manuel <luisyosemite@gmail.com>
2018-12-02 23:17:07 -0600

    init basico

---
+++ 01-server/dist/sockets/socket.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const usuarios_lista_1 = require("../classes/usuarios-lista");
+const usuario_1 = require("../classes/usuario");
+exports.usuariosConectados = new usuarios_lista_1.UsuariosLista();
+exports.conectarCliente = (cliente) => {
+    const usuario = new usuario_1.Usuario(cliente.id);
+    exports.usuariosConectados.agregar(usuario);
+};
+exports.desconectar = (cliente) => {
+    cliente.on('disconnect', () => {
+        console.log('Cliente desconectado');
+        exports.usuariosConectados.borrarUsuario(cliente.id);
+    });
+};
+// Escuchar mensajes
+exports.mensaje = (cliente, io) => {
+    cliente.on('mensaje', (payload) => {
+        console.log('Mensaje recibido', payload);
+        io.emit('mensaje-nuevo', payload);
+    });
+};
+// Configurar usuario
+exports.configurarUsuario = (cliente, io) => {
+    cliente.on('configurar-usuario', (payload, callback) => {
+        exports.usuariosConectados.actualizarNombre(cliente.id, payload.nombre);
+        callback({
+            ok: true,
+            mensaje: `Usuario ${payload.nombre}, configurado`
+        });
+    });
+};
---
+++ 02-basico/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
+import { ChatService } from './services/chat.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(
+    public wsService: WebsocketService,
+    public chatServie: ChatService
+  ) { }
+
+  ngOnInit() {
+
+    this.chatServie.getMessagesPrivate().subscribe( msg => {
+
+      console.log(msg);
+
+    });
+
+  }
+
+}
